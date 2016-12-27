0553b7e0535a54e0a1b47a6f20dfd59db5ed6053
newind <xinfengzaiwangyi@163.com>
2016-12-27 22:45:21 +0800

    save

--- python/tiny_ftp.py
+++ python/tiny_ftp.py
@@ -1,15 +1,45 @@
 import os
 import sys
 import socket
-import thread
+import threading,time,signal
+import ConfigParser
 from SocketServer import *
 
-class FtpConfig:
-    RootPath = "."
-    AuthConfig = "./auth.config"
+class FtpApp(threading.Thread):
+    TheFtpApp = None
+    def __init__(self):
+        threading.Thread.__init__(self)
+        TheFtpApp = self
+    def start(self):
+        if os.path.isfile(AuthConfig):
+            print "config file not exists " , AuthConfig
+            return False
+        try:
+            cf = ConfigParser.ConfigParser()
+            cf.read(AuthConfig)
+            self.ip = cf.get("ftp","ip")
+            self.port = cf.getint("ftp","port")
+            self.root = cf.get("ftp","root")
+            self.user_name = cf.get("user","name")
+            self.pass_word = cf.get("user","password")
+            self.server = SocketServer.ThreadingTCPServer((ip,port),FtpSesstion)
+            threading.Thread.start(self)
+        except Exception as e:
+            print e
+        else:
+            pass
+        finally:
+            pass
+
+    def run(self):
+        self.server.serve_forever()
+
+    def stop(self):
+        pass
     @staticmethod
-    def loadConfig():
-        
+    def DebugQuit(num,frame):
+        TheFtpApp.stop()
+
 
 class FtpSesstion(StreamRequestHandler):
 
