45d702ccf2dab6d73e0f5bd7a194a1215c0d8271
newind <xinfengzaiwangyi@163.com>
2017-01-12 21:46:43 +0800

    changes on windows.

--- python/tiny_ftp.py
+++ python/tiny_ftp.py
@@ -3,57 +3,74 @@ import sys
 import socket
 import threading,time,signal
 import ConfigParser
-from SocketServer import *
+import SocketServer
 
-class FtpApp(threading.Thread):
-    TheFtpApp = None
+class FtpApp:
+    AuthConfig = "./ftp.conf"
     def __init__(self):
-        threading.Thread.__init__(self)
-        TheFtpApp = self
+        pass
     def start(self):
-        if os.path.isfile(AuthConfig):
-            print "config file not exists " , AuthConfig
+        if not os.path.isfile(FtpApp.AuthConfig):
+            print "config file not exists " , FtpApp.AuthConfig
             return False
         try:
             cf = ConfigParser.ConfigParser()
-            cf.read(AuthConfig)
+            cf.read(FtpApp.AuthConfig)
             self.ip = cf.get("ftp","ip")
             self.port = cf.getint("ftp","port")
             self.root = cf.get("ftp","root")
+            self.writeable = cf.get("ftp","writeable") == "True"
             self.user_name = cf.get("user","name")
             self.pass_word = cf.get("user","password")
-            self.server = SocketServer.ThreadingTCPServer((ip,port),FtpSesstion)
-            threading.Thread.start(self)
+            self.server = SocketServer.ThreadingTCPServer((self.ip,self.port),FtpSesstion)
+            #threading.Thread.start(self)
         except Exception as e:
             print e
-        else:
-            pass
+            return False
         finally:
-            pass
+            return True
 
     def run(self):
         self.server.serve_forever()
 
     def stop(self):
-        pass
+        def do_stop(self):
+            print "shutdown call."
+            self.server.shutdown()
+            print "shutdown called."
+        threading._start_new_thread(do_stop,(self,))
+
     @staticmethod
     def DebugQuit(num,frame):
-        TheFtpApp.stop()
+        print "about to close"
+        theApp.stop()
+    def isWirteAble(self):
+        return self.writeable
+    def changeDirectory(self,subDir):
+        tar = self.root + subDir
+        tar = tar.replace("//","/")
+        tar = tar.replace("/../","/")
+        return tar
 
-
-class FtpSesstion(StreamRequestHandler):
-
+class FtpSesstion(SocketServer.StreamRequestHandler):
     def __init__(self):
         self.user_name = ""
         self.root_path = ""
+        self.app = FtpApp.TheFtpApp
         pass
     def handle(self):
+        line = self.rfile.readline()
+        print line
+        self.request.close()
 
-        pass
-
-
+theApp = FtpApp()
 def main():
-    print FtpConfig.RootPath , FtpConfig.AuthConfig
+    if theApp.start():
+        signal.signal(signal.SIGINT,FtpApp.DebugQuit)
+        signal.signal(signal.SIGTERM,FtpApp.DebugQuit)
+        theApp.run()
+    else:
+        print "ftp app start fail."
 
 if __name__ == "__main__":
     main()
