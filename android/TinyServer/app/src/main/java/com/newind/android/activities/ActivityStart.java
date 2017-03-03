d1481ad7fbe03b2e9fb76ee968bfa4bd842b0cf8
newind <xinfengzaiwangyi@163.com>
2017-03-03 21:50:46 +0800

    lost files.

--- android/TinyServer/app/src/main/java/com/newind/android/activities/ActivityStart.java
+++ android/TinyServer/app/src/main/java/com/newind/android/activities/ActivityStart.java
@@ -1,16 +1,22 @@
 package com.newind.android.activities;
 
+import android.animation.Animator;
+import android.animation.AnimatorInflater;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.newind.android.ApplicationMain;
 import com.newind.android.R;
 
 public class ActivityStart extends Activity {
-    private static final long MIN_START_TIME = 3 * 1000L;
+    private static final long MIN_START_TIME = 2 * 1000L;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,10 +27,14 @@ public class ActivityStart extends Activity {
             startMainDelay();
         }else {
             startMain();
-        };
+        }
     }
 
     private void startMainDelay() {
+        ImageView iv_logo = (ImageView) findViewById(R.id.iv_logo);
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.routing);
+        animation.setRepeatCount(Animation.INFINITE);
+        iv_logo.startAnimation(animation);
         new Thread(new Runnable() {
             @Override
             public void run() {
