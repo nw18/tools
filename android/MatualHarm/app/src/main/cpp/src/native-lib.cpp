1445e640fd7f1b6b8106c9e908acfdef24852a10
newind <xinfengzaiwangyi@163.com>
2017-03-29 22:34:14 +0800

    readd manual harm

--- android/MatualHarm/app/src/main/cpp/src/native-lib.cpp
+++ android/MatualHarm/app/src/main/cpp/src/native-lib.cpp
@@ -6,6 +6,6 @@ JNIEXPORT jstring JNICALL
 Java_com_newind_mutualharm_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++" + test();
+    std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
